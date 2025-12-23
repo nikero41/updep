81ba038be23305a7a3396e5198fe694775000872
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-12-23 01:31:04 +0100

    Use long npm flag to get more info, for the future

--- pkg/packageManager/adapters/npm.go
+++ pkg/packageManager/adapters/npm.go
@@ -23,7 +23,7 @@ type JSONPackage struct {
 }
 
 func (pm Npm) GetOutdated() ([]packages.Package, error) {
-	output, err := exec.Command("npm", "outdated", "--json").Output()
+	output, err := exec.Command("npm", "outdated", "--json", "--long").Output()
 	// output, err := os.ReadFile("output.json")
 
 	// NOTE: npm outdated returns exit status 1 if there are outdated packages
