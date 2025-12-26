7fc9f31a99f50519367e23738023283179ce8a8e
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-12-26 17:22:08 +0100

    Create color pallete with miningful colors

--- pkg/pkgTable/styles.go
+++ pkg/pkgTable/styles.go
@@ -10,9 +10,8 @@ var (
 	headerStyle    = lipgloss.NewStyle().Bold(true).Underline(true)
 	activeRowStyle = lipgloss.NewStyle().
 			Bold(true).
-			Background(config.Theme.Surface1)
-	majorDiffStyle = lipgloss.NewStyle().Foreground(config.Theme.Red)
-	minorDiffStyle = lipgloss.NewStyle().
-			Foreground(config.Theme.Peach)
-	patchDiffStyle = lipgloss.NewStyle().Foreground(config.Theme.Green)
+			Background(config.Theme.ActiveBackground)
+	majorDiffStyle = lipgloss.NewStyle().Foreground(config.Theme.Major)
+	minorDiffStyle = lipgloss.NewStyle().Foreground(config.Theme.Minor)
+	patchDiffStyle = lipgloss.NewStyle().Foreground(config.Theme.Patch)
 )
