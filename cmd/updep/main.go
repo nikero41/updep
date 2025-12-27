b51e7d9a56ee188e4c0e523279aa8c331e627252
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2025-12-27 21:16:42 +0100

    Log into a file

--- cmd/updep/main.go
+++ cmd/updep/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"updep/pkg/app"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
 	p := tea.NewProgram(app.New())
 	defer p.Kill()
 	if _, err := p.Run(); err != nil {
